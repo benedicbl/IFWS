e384c097357ca0ab515dd7edfed63e1a0a2ee7ed
Benedictus Benny Lesmana <67849416+benedicbl@users.noreply.github.com>
2025-12-11 10:28:02 +0700

    Folder Includes

---
+++ includes/config.php
@@ -0,0 +1,20 @@
+<?php
+date_default_timezone_set('Asia/Jakarta');
+
+// Mulai session di file config agar tidak lupa
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Pengaturan koneksi database
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db   = 'ifws_db';
+
+$koneksi = mysqli_connect($host, $user, $pass, $db);
+
+if (!$koneksi) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+?>
